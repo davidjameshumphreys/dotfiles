3b8b25cd126325583cca692b3325ddfeeb994446
David James Humphreys <davidjameshumphreys@gmail.com>
2014-03-22 17:12:09 +0000

    Cleaned up dotfiles for vagrant.

---
+++ .lein/profiles.clj
@@ -0,0 +1,11 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.4"]
+                  [lein-noir "1.2.1"]
+                  [lein-gorilla "0.1.2"]
+                  [lein-pprint "1.1.1"]
+                  ]}
+
+ :profiles {:dev {:dependencies [[alembic "0.1.1"]]}}
+ }

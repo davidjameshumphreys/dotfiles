e518cc3a2623815dc4ddb147e4a431c10ccf6a7c
David James Humphreys <davidjameshumphreys@gmail.com>
2014-04-07 15:27:13 +0000

    Updated the start file

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 H=/home/david
-for i in .ackrc .gitconfig .tmux.conf; do
+for i in .ackrc .gitconfig .tmux.conf .zshrc; do
     cp $i ${H}/$i
 done
 
+cp -r .oh-my-zsh ${H}/.oh-my-zsh
 cp -r .lein ${H}/.lein
 cp -r .emacs.d ${H}/.emacs.d

7b01593e22a6e04d558e3646b004ff4511fc450a
David James Humphreys <davidjameshumphreys@gmail.com>
2014-04-29 10:40:51 +0000

    CLJ profile and adding my own bashrc code.

---
+++ bashrc
@@ -0,0 +1,7 @@
+################################
+# david's bashrc
+################################
+# track a remote branch
+alias track='git checkout -t'
+alias g='git'
+alias gl='git lola'
--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ for i in .ackrc .gitconfig .tmux.conf .zshrc; do
     cp $i ${H}/$i
 done
 
+mv ${H}/.bashrc ${H}/_bashrc
+cat ${H}/_bashrc bashrc > ${H}/.bashrc
+
 cp -r .oh-my-zsh ${H}/.oh-my-zsh
-cp -r .lein ${H}/.lein
+cp -r .lein/* ${H}/.lein
 cp -r .emacs.d ${H}/.emacs.d

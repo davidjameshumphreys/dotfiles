7984ba06cbf64f2d46b9c1696cf80d220c677a7e
David James Humphreys <davidjameshumphreys@gmail.com>
2014-04-30 18:15:12 +0000

    Removed zsh from the start script.

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 H=/home/david
 
 #rsync --filter='- .git' --filter='- start.sh' -av . $H
-for i in .ackrc .gitconfig .tmux.conf .zshrc .bash_aliases; do
+for i in .ackrc .gitconfig .tmux.conf .bash_aliases; do
     cp $i ${H}/$i
 done
 
-cp -r .oh-my-zsh ${H}/.oh-my-zsh
 cp -r .lein/* ${H}/.lein
 cp -r .emacs.d ${H}/.emacs.d

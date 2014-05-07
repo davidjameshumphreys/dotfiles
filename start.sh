2d056bd4b7cb1b0024a63593b54aadb6a5b4d750
David James Humphreys <davidjameshumphreys@gmail.com>
2014-05-07 16:33:57 +0000

    Fixed start script and adding mark-tools

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 H=/home/david
 
 #rsync --filter='- .git' --filter='- start.sh' -av . $H
-for i in .ackrc .gitconfig .tmux.conf .bash_aliases; do
-    ln -s $i ${H}/$i
+for i in .gitconfig .tmux.conf .bash_aliases; do
+    ln -fs  "${PWD}/$i" "${H}/$i"
 done
 
 cp -r .lein/* ${H}/.lein
-ln -s .emacs.d ${H}/.emacs.d
+ln -fs "${PWD}/.emacs.d" "${H}/.emacs.d"

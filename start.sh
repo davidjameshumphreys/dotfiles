d9d7f06938d6417b94d083f115010edf78ad6a3a
David James Humphreys <davidjameshumphreys@gmail.com>
2014-06-05 16:38:31 +0100

    Added rainbarf to tmux.

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 H=/home/david
 
 #rsync --filter='- .git' --filter='- start.sh' -av . $H
-for i in .gitconfig .tmux.conf .bash_aliases; do
+for i in .gitconfig .tmux.conf .bash_aliases .zshrc .oh-my-zsh .emacs.d; do
     ln -fs  "${PWD}/$i" "${H}/$i"
 done
 
 cp -r .lein/* ${H}/.lein
 cp .ssh/config ${H}/.ssh/config
-ln -fs "${PWD}/.emacs.d" "${H}/.emacs.d"
+#ln -fs "${PWD}/.emacs.d" "${H}/.emacs.d"
+
+sudo apt-get -y install cpanminus
+sudo cpanm git://github.com/creaktive/rainbarf.git

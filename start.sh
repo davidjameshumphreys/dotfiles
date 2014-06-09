6dba87dcadae85823475ecc8c55176e1d46c3254
David James Humphreys <davidjameshumphreys@gmail.com>
2014-06-09 10:24:04 +0100

    Startup script tweaking

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 H=/home/david
 
-#rsync --filter='- .git' --filter='- start.sh' -av . $H
-for i in .gitconfig .tmux.conf .bash_aliases .zshrc .oh-my-zsh .emacs.d; do
+for i in .gitconfig .tmux.conf .bash_aliases .zshrc .oh-my-zsh .emacs.d .lein/profiles.clj .lein/init.clj .ssh/config; do
     ln -fs  "${PWD}/$i" "${H}/$i"
 done
 
-cp -r .lein/* ${H}/.lein
-cp .ssh/config ${H}/.ssh/config
-#ln -fs "${PWD}/.emacs.d" "${H}/.emacs.d"
-
-sudo apt-get -y install cpanminus
-sudo cpanm git://github.com/creaktive/rainbarf.git
+cpanm || sudo apt-get -y install cpanminus
+rainbarf || sudo cpanm git://github.com/creaktive/rainbarf.git

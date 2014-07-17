7c7688c886eadb3382c8b188d811d476f6e5ebda
David James Humphreys <davidjameshumphreys@gmail.com>
2014-07-17 11:46:27 +0100

    Quick hack to reduce the output for the start script if things are aleady installed.

--- start.sh
+++ start.sh
@@ -4,9 +4,14 @@ for i in .gitconfig .tmux.conf .bash_aliases .zshrc .oh-my-zsh .emacs.d .lein/pr
     ln -fs  "${PWD}/$i" "${H}/$i"
 done
 
-cpanm 2> /dev/null || sudo apt-get -y install cpanminus
-rainbarf 2> /dev/null || sudo cpanm git://github.com/creaktive/rainbarf.git
-sudo apt-get -y -qq install python-pip
-sudo apt-get -y -qq install python-virtualenv
-sudo apt-get -y -qq install virtualenvwrapper
-sudo apt-get -y -qq install golang
+if [ "$1" = 'install' ] ; then
+    dpkg -l cpanminus 2>&1         | grep -sq '^ii' || sudo apt-get -y -qq install cpanminus
+    dpkg -l python-pip 2>&1        | grep -sq '^ii' || sudo apt-get -y -qq install python-pip
+    dpkg -l python-virtualenv 2>&1 | grep -sq '^ii' || sudo apt-get -y -qq install python-virtualenv
+    dpkg -l virtualenvwrapper 2>&1 | grep -sq '^ii' || sudo apt-get -y -qq install virtualenvwrapper
+    dpkg -l golang 2>&1            | grep -sq '^ii' || sudo apt-get -y -qq install golang
+
+    which rainbarf 2>&1 > /dev/null || sudo cpanm git://github.com/creaktive/rainbarf.git
+else
+    echo "Skipping install"
+fi

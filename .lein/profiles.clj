4bf171d2e6d1cdaeaee968906db3d062c95a4bb2
David James Humphreys <davidjameshumphreys@gmail.com>
2014-08-01 16:13:42 +0100

    Added humane-test-output to CLJ settings:
    https://github.com/pjstadig/humane-test-output

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]}
- }
+{:user {:plugins      [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        :injections   [(require 'pjstadig.humane-test-output)
+                       (pjstadig.humane-test-output/activate!)]}}

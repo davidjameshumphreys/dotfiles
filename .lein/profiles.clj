2a90b4f1e7a28f0be2e66e5419e6178c1d02a030
David James Humphreys <davidjameshumphreys@gmail.com>
2014-09-26 17:22:11 +0100

    Redo lein profiles (+ cider 8)

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.7.0"]
-                  [jonase/eastwood "0.1.3"]
-                  [lein-ancient "0.5.5"]
-                  [lein-cljsbuild "1.0.3"]
-                  [lein-cloverage "1.0.2"]
-                  [lein-kibit "0.0.8"]
-                  [lein-release "1.0.5"]
-                  [lein-try "0.4.1"]
-                  [lein-vanity "0.2.0"]]
-        :dependencies [[clojure-complete "0.2.3"]
-                       [criterium "0.4.3"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [clojure-complete "0.2.3"]
+                       [criterium "0.4.3"]]
+        :injections   [(require 'pjstadig.humane-test-output)
+                       (pjstadig.humane-test-output/activate!)]}}

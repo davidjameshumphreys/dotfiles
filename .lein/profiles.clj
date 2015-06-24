c4a15ae1c75daaeae328169f5ea4340ffdc61719
David James Humphreys <davidjameshumphreys@gmail.com>
2015-06-24 15:10:37 +0100

    lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,10 @@
-{:user {:plugins      [[cider/cider-nrepl "0.9.0-SNAPSHOT" :exclusions [org.clojure/clojure
-                                                                        org.clojure/tools.nrepl]]
-                       [lein-hiera "0.9.0"]]
+{:user {:plugins      [[cider/cider-nrepl "0.9.0" :exclusions [org.clojure/clojure
+                                                               org.clojure/tools.nrepl]]
+                       [lein-hiera "0.9.0"]
+                       [refactor-nrepl "1.0.5"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
-                       [criterium "0.4.3"]]
+                       [criterium "0.4.3"]
+                       [org.clojure/tools.nrepl
+                        "0.2.10" :exclusions [org.clojure/clojure]]]
         :injections   [(require 'pjstadig.humane-test-output)
                        (pjstadig.humane-test-output/activate!)]}}

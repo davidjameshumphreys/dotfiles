d8250638cdc1d235164e7680c22c2410af4c175c
David James Humphreys <davidjameshumphreys@gmail.com>
2015-12-31 22:36:18 +0000

    Lein pprint plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,8 @@
                         :exclusions [org.clojure/clojure
                                      org.clojure/tools.nrepl]]
                        [lein-hiera "0.9.0"]
-                       [refactor-nrepl "1.1.0"]]
+                       [refactor-nrepl "1.1.0"]
+                       [lein-pprint "1.1.2"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [criterium "0.4.3"]
                        [org.clojure/tools.nrepl

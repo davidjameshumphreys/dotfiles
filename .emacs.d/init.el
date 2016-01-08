ee2fa5618ae41ccb18a23f31cf6ca2996c78d084
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-08 09:35:32 +0000

    Switch to echo-bell-mode to avoid the annoying delay in the visual bell.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -543,6 +543,7 @@ If called with a prefix, prompts for flags to pass to ag."
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(echo-bell-background "dark red")
  '(git-commit-finish-query-functions nil)
  '(org-confirm-babel-evaluate nil)
  '(projectile-globally-ignored-directories
@@ -559,16 +560,6 @@ If called with a prefix, prompts for flags to pass to ag."
 (setq ag-arguments '("--ignore=*build/js*" "--line-number" "--smart-case" "--nogroup" "--column" "--"))
 (unbind-key "C-z")
 
-;; http://www.emacswiki.org/emacs/AlarmBell#toc11
-(defun mode-line-visible-bell ()
-  "A friendlier visual bell effect."
-  (invert-face 'mode-line)
-  (run-with-timer 0.3 nil 'invert-face 'mode-line))
-;;TODO: for the C-<- and C-->  do a bell first.
-
-(setq visible-bell nil
-      ring-bell-function 'mode-line-visible-bell)
-
 (maybe-install-and-require 'god-mode)
 (diminish 'god-local-mode "[[☁️ GOD ⚡️ ]]")
 
@@ -581,7 +572,6 @@ If called with a prefix, prompts for flags to pass to ag."
 (bind-key "C-c C-q" 'cider-quit clojure-mode-map)
 
 (maybe-install-and-require 'mmm-mode)
-
 
 (-map
  (lambda (m)
@@ -669,3 +659,6 @@ If called with a prefix, prompts for flags to pass to ag."
    (sh . t)
    ;(pony . t)
    ))
+
+(maybe-install-and-require 'echo-bell)
+(echo-bell-mode t)

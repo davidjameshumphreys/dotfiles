39224f5be84449d4bdd1b6e461e418784eb2d2bf
David James Humphreys <davidjameshumphreys@gmail.com>
2016-01-17 22:46:01 +0000

    Bump refactor-nrepl, remove hiera.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,7 @@
 {:user {:plugins      [[cider/cider-nrepl "0.10.1"
                         :exclusions [org.clojure/clojure
                                      org.clojure/tools.nrepl]]
-                       [lein-hiera "0.9.0"]
-                       [refactor-nrepl "1.1.0"]
+                       [refactor-nrepl "1.2.0"]
                        [lein-pprint "1.1.2"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [criterium "0.4.3"]
